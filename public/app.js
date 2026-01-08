ec9eb17b1036650b3fc55b82a4c62c932ea5e635
Alikhan812107 <amd812107@gmail.com>
2026-01-09 02:59:37 +0500

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,47 @@
+const btn = document.getElementById("loadBtn");
+const content = document.getElementById("content");
+
+btn.addEventListener("click", async () => {
+  content.innerHTML = "Loading...";
+
+  const res = await fetch("/api/profile");
+  const data = await res.json();
+
+  content.innerHTML = `
+    <div class="card">
+      <img src="${data.user.picture}" />
+      <h2>${data.user.firstName} ${data.user.lastName}</h2>
+      <p><b>Gender:</b> ${data.user.gender}</p>
+      <p><b>Age:</b> ${data.user.age}</p>
+      <p><b>City:</b> ${data.user.city}</p>
+      <p><b>Country:</b> ${data.user.country}</p>
+      <p><b>Address:</b> ${data.user.address}</p>
+    </div>
+
+    <div class="card">
+      <h3>Country Info</h3>
+      <img src="${data.country.flag}" width="80" />
+      <p><b>Capital:</b> ${data.country.capital}</p>
+      <p><b>Languages:</b> ${data.country.languages.join(", ")}</p>
+      <p><b>Currency:</b> ${data.country.currency}</p>
+    </div>
+
+    <div class="card">
+      <h3>Exchange Rates</h3>
+      <p>1 ${data.exchangeRates.base} = ${data.exchangeRates.USD} USD</p>
+      <p>1 ${data.exchangeRates.base} = ${data.exchangeRates.KZT} KZT</p>
+    </div>
+
+    <div class="card">
+      <h3>News</h3>
+      ${data.news.map(n => `
+        <div class="news">
+          <h4>${n.title}</h4>
+          ${n.image ? `<img src="${n.image}" />` : ""}
+          <p>${n.description || ""}</p>
+          <a href="${n.url}" target="_blank">Read more</a>
+        </div>
+      `).join("")}
+    </div>
+  `;
+});
---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+
+app.get("/api/profile", async (req, res) => {
+  try {
+
+// random user
+    const userRes = await axios.get("https://randomuser.me/api/");
+    const user = userRes.data.results[0];
+
+    const cleanedUser = {
+      firstName: user.name.first,
+      lastName: user.name.last,
+      gender: user.gender,
+      age: user.dob.age,
+      dob: user.dob.date,
+      picture: user.picture.large,
+      city: user.location.city,
+      country: user.location.country,
+      address: `${user.location.street.name} ${user.location.street.number}`
+    };
+
+// country info
+    const countryRes = await axios.get(
+      `https://restcountries.com/v3.1/name/${cleanedUser.country}`
+    );
+
+    const country = countryRes.data[0];
+    const currencyCode = country.currencies
+      ? Object.keys(country.currencies)[0]
+      : null;
+
+    const cleanedCountry = {
+      name: country.name.common,
+      capital: country.capital ? country.capital[0] : "N/A",
+      languages: country.languages
+        ? Object.values(country.languages)
+        : [],
+      currency: currencyCode || "N/A",
+      flag: country.flags.png
+    };
+
+   // Exchange rates
+
+    let exchangeRates = null;
+
+    if (currencyCode) {
+      const exchangeRes = await axios.get(
+        `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_API_KEY}/latest/${currencyCode}`
+      );
+
+      exchangeRates = {
+        base: currencyCode,
+        USD: exchangeRes.data.conversion_rates.USD,
+        KZT: exchangeRes.data.conversion_rates.KZT
+      };
+    }
+
+   // News API
+    const newsRes = await axios.get(
+      `https://newsapi.org/v2/everything?q=${cleanedUser.country}&language=en&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`
+    );
+
+    const news = newsRes.data.articles.map(article => ({
+      title: article.title,
+      description: article.description,
+      image: article.urlToImage,
+      url: article.url
+    }));
+
+   
+    //Final response
+
+    res.json({
+      user: cleanedUser,
+      country: cleanedCountry,
+      exchangeRates,
+      news
+    });
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to load profile data" });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
